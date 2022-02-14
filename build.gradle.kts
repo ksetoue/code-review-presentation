1cb100ce69f46a0145ef5c82e2aae05f9e21022a
ksetoue <karoline.kimiko@gmail.com>
2022-02-14 18:04:33 -0300

    :heavy_plus_sign: add postgres, jdbc and flyway dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.blinkmob.flywayrename") version "1.0.0"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 }
@@ -19,11 +20,17 @@ extra["springCloudVersion"] = "2021.0.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+	implementation("org.flywaydb:flyway-core:8.4.3")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	
+	runtimeOnly("org.postgresql:postgresql")
+	
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
