b1c27840b8e11b884f95f889b7ece27e7fddb056
ksetoue <karoline.kimiko@gmail.com>
2022-02-14 17:00:06 -0300

    :tada: initial commit

---
+++ src/test/kotlin/com/coderv/presentation/PresentationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coderv.presentation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PresentationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
